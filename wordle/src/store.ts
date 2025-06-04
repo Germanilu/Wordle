b4159197876265041cf6bbfcb2dbfd83a5e48d92
Luciano Germani <lucianogermani93@gmail.com>
2025-06-04 17:36:02 +0200

    Feat: Getting GameInfo data once created

--- wordle/src/store.ts
+++ wordle/src/store.ts
@@ -1,6 +1,21 @@
 import { create } from 'zustand';
 import axios from 'axios'
+import { devtools } from 'zustand/middleware'
 
+
+
+type GameInfo = {
+    gameId: string;
+    status: string;
+    attemptsLeft: number;
+    wordToGuess: string;
+    guesses: [];
+}
+
+type WordleStore = {
+    gameInfo: GameInfo
+    createGameId: () => Promise<void>
+}
 
 const createGame = async () => {
     try {
@@ -16,10 +31,36 @@ const createGame = async () => {
 }
 
 
-export const useWordleStore = create(() => ({
+const getGameInfo = async (gameId: string) => {
+    try {
+        const url = `http://localhost:3000/game/${gameId}`
+        const response = await axios.get(url)
+        if (response.status === 200) {
+            return response.data
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+export const useWordleStore = create<WordleStore>()(devtools((set) => ({
+
+     gameInfo: {
+        guesses: [],
+        gameId: "",
+        status: "",
+        attemptsLeft: 5,
+        wordToGuess: ""
+    },
 
     createGameId: async() => {
         const {gameId} = await createGame()
-        console.log(gameId)
+        if (gameId) {
+            const gameInfo = await getGameInfo(gameId)
+            set(() => ({
+                gameInfo
+            }))
+        }
     }
-}))
+})))
